f2552f1577658193ccbdcc6cc8c3ce84426d5a8d
VarunSambanni <87132174+VarunSambanni@users.noreply.github.com>
2022-01-20 11:52:36 +0530

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize') ; 
 
-const sequelize = new Sequelize('sql6465786', 'sql6465786', process.env.DATABASE_PASSWORD, {dialect: 'mysql', host: process.env.DATABASE_HOST,define: {
+const sequelize = new Sequelize( process.env.DATABASE_USERNAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {dialect: 'mysql', host: process.env.DATABASE_HOST,define: {
     timestamps: false 
 }});
 
